cd87fc5dcf28e0176709777c5a6d7201c3aeeb6e
DeftArtisan <81402620+DeftArtisan@users.noreply.github.com>
2022-07-13 21:42:34 +0300

    Update functionality.js

--- functionality.js
+++ functionality.js
@@ -1,1 +1,12 @@
 
+document.onscroll = () => {
+    if(window.scrollY > 0){
+        document.querySelector('.header').classList.add('active')       
+    } else {
+        document.querySelector('.header').classList.remove('active') 
+    }
+}
+
+document.querySelector('.homesection .btn').addEventListener('click', function() {
+    alert('Submission functionality remains to be implemented')
+})
